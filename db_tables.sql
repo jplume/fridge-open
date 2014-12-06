172e45b23973dd3fb7cfaa8e4562f414cf3bb580
Janis Plume <janis.plume@tieto.com>
2014-12-06 22:09:38 +0200

    database structure

---
+++ db_tables.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE event
+(
+  id bigint NOT NULL,
+  event_type character varying(10),
+  date_from timestamp without time zone,
+  CONSTRAINT pk1 PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
